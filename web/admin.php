dae68b95ed38dd4a340572cbf30ec0ed249622d8
mvolchkov <m.volchkov@be-interactive.ru>
2017-06-18 14:46:30 +0300

    new

--- web/admin.php
+++ web/admin.php
@@ -1,9 +1,9 @@
 <?php
-    
     session_name('admin-panel');
 
     /** @const Название проекта */
     define('NOMVC_APPNAME', 'admin');
+
     /** @const Базовая папка проекта */
     define('NOMVC_BASEDIR', dirname(dirname(__FILE__)));
     
@@ -15,8 +15,8 @@
     require_once(dirname(__DIR__).'/lib/autoload.php');
     
     try {
-//        $context = new Context(Context::ENV_PROD);
-        $context = new Context(Context::ENV_DEBUG);
+        $context = new Context(Context::ENV_PROD);
+//        $context = new Context(Context::ENV_DEBUG);
         echo $context->getRootController()->run();
     } catch (Exception $ex) {
         echo $ex->getMessage();
